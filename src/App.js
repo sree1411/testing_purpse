9ef8d09aef8588781770c3b8de9fc5db58883660
unknown <sreekanth>
2024-07-25 18:17:32 +0530

    life cycle example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+
 
 
 function App() {
+
+
+  const [count,setCount] = useState(0);
+  
+  
+  useEffect(()=>{
+    //mounting 
+    console.log("mounting")
+    return ()=>{
+      console.log("unmounting")
+    }
+  },[])
+   
+
+  useEffect(()=>{
+    //updating
+    console.log("updating")
+  },[count])
+
+
   return (
     <div>
 
-      <h1> Sample to the application </h1> 
+      <h1> Life cycle feature example: </h1> 
+      <p> {count }</p>
+      <button onClick={()=>setCount(count+1)} > inc </button>
        
     </div>
   )
 }
 
-export default App
+  
+
+function Parent(){
+
+ const [show, setShow] = useState(true)
+
+  return(
+    <>
+      <button onClick={()=>setShow(!show)} > {show ? "hide" :"un-hide"} </button>
+      {
+        show  && <App/>
+      }
+    </>
+  )
+}
+export default Parent;
+
+
