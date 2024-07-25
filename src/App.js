b429c783b9608add78651fab10abaab757fd5cf3
unknown <sreekanth>
2024-07-25 18:29:27 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+
+const reducer =(state,action)=>{
+   switch(action.type){
+       case "INCREMENT":
+        return {count:state.count + 1}
+      case "DECREMENT":
+        return {count:state.count - 1}
+        default:
+          return {state}
+   }
+}
+
+
+
+const initialState = {count:0}
 
 
 function App() {
+
+ const [state,dispatch] = useReducer(reducer,initialState)
+
+
   return (
     <div>
 
-      <h1> Sample to the application </h1> 
+      <h1> user reducer to the application </h1> 
+
+      <p>  count :{state.count} </p>
+
+      <button onClick={()=>dispatch({type:"INCREMENT"})} > INCREMENT </button>
+
        
     </div>
   )
