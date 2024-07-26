0d9c469e4fa74c366cd36dfd370db8d9291fc2aa
unknown <sreekanth>
2024-07-26 21:19:52 +0530

    usecallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const incBtn = useCallback(()=>{
+      setCount(prev => prev + 1)
+  },[])
+
+  const decBtn = useCallback(()=>{
+    setCount(prev => prev >0 ? prev - 1 : prev)
+    
+},[])
+
+const resetBtn = useCallback(()=>{
+  setCount(0)
+},[])
+
+
   return (
     <div>
 
-      <h1> Sample to the application </h1> 
+      <h1> usecall back example </h1> 
+      <p>  {count}</p>
+       <button onClick={incBtn} >inc </button>
+       <button onClick={decBtn} >dec </button>
+       <button onClick={resetBtn} >reset </button>
        
     </div>
   )
