181f1bc2e5f4f02f219fba805959448aa704856b
unknown <sreekanth>
2024-07-26 17:40:45 +0530

    create done and undone task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './App.css'
 
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('')
+
+
+  const handleChange =(e)=>{
+     setNewUser(e.target.value)
+  }
+
+  const addDetails = ()=>{
+    setUsers([...users, {name:newUser, status:false}]);
+    setNewUser('')
+  }
+
+  const changStatus =(i)=>{
+       var temp= [...users]
+       temp[i].status = !temp[i].status;
+       setUsers(temp)
+  }
+
+
   return (
     <div>
 
       <h1> Sample to the application </h1> 
+
+       <input type='text' value={newUser} onChange={handleChange}  />
+       <button onClick={addDetails} > Add details </button>
+
+      {
+        users.map((user,i)=>(
+            <p className={user.status ? "mark":"unmark"} > 
+              {user.name}
+              <button onClick={()=>changStatus(i)} > {user.status ? "Undone":"Done"} </button>
+            </p>
+        ))
+      }
        
     </div>
   )
