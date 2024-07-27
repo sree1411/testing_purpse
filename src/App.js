caf82b620cfaa1f4ad7caaaf4a60f5774ac8c2cc
unknown <sreekanth>
2024-07-27 11:19:27 +0530

    created start stop reset timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState,useRef } from 'react'
 
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [isrunning, setIsRunning] = useState(false);
+  const intervalRef = useRef()
+
+  const startBtn =()=>{
+     if(!isrunning){
+       setIsRunning(true);
+       intervalRef.current = setInterval(()=>{
+         setCount((prev)=> prev + 1)
+       },1000)
+     }
+  }
+
+  const stopBtn =()=>{
+    setIsRunning(false);
+    clearInterval(intervalRef.current )
+  }
+
+  const resetBtn =()=>{
+    setIsRunning(false);
+    clearInterval(intervalRef.current )
+    setCount(0)
+  }
+
+
   return (
     <div>
 
-      <h1> Sample to the applications </h1> 
+      <h1> Timer Example :{count}</h1> 
+       
+      <button onClick={startBtn} > start </button>
+      <button onClick={stopBtn}> stop</button>
+      <button onClick={resetBtn}> reset </button>
+
        
     </div>
   )
