25b4d73ae84dc78b36eeb7b2d88528047a192280
unknown <sreekanth>
2024-07-28 22:21:27 +0530

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import useLocalStorage from './useLocalStorage'
 
 function App() {
+
+  
+  const [name, setName] = useLocalStorage('username', '')
+  const [id, setId] = useLocalStorage('id', '')
+ 
+
   return (
     <div>
 
-      <h1> Sample to the applications </h1> 
+      <h1> Sample to the applicationsssssssssss </h1> 
+       <h1>{name}</h1>
+       <input type='text' value={name} onChange={(e)=>setName(e.target.value)}   />
+       <input type='text' value={id} onChange={(e)=>setId(e.target.value)}   />
        
     </div>
   )
