e0f93053ecb1fe88477304bbf57ea0d1f57a37b7
unknown <sreekanth>
2024-07-28 20:50:39 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
 
 function App() {
+
+   const data = useRef()
+
+  const handleSubmit = (e)=>{
+       e.preventDefault()
+  }
+
+  useEffect(()=>{
+    data.current.focus()
+  },[])
+
   return (
     <div>
 
-      <h1> Sample to the applications </h1> 
+      <h1> useRef Example</h1> 
+      <form onSubmit={handleSubmit}>
+         <input type="text" ref={data}  />
+         <input type='submit' />
+      </form>
        
     </div>
   )
