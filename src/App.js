4dbf58f53c1119bffef5312c3501a951ce7a3d07
unknown <sreekanth>
2024-08-04 11:05:28 +0530

    forward_ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import ChildComponent from './ChildComponent'
 
 
 function App() {
+
+ 
+   const inputRef = useRef(null)
+
+
   return (
     <div>
 
       <h1> Sample to the applications </h1> 
+      <ChildComponent ref={inputRef} />
+      <button onClick={()=>inputRef.current.focus()} >  focus</button>
        
     </div>
   )
---
+++ src/ChildComponent.jsx
@@ -0,0 +1,7 @@
+import React, { forwardRef } from 'react'
+
+const ChildComponent = forwardRef((props, ref)=>{
+    return <input ref={ref} />
+})
+
+export default ChildComponent
